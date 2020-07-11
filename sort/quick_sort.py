5d96d86ecf3b2d7888093455d5535f5d31f3e556
pinchiachen <chenargar@gmail.com>
2020-07-11 23:05:20 +0800

    Bulbasaur.

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -2,8 +2,13 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     pivot = arr.pop(0)
-    less_arr = [num for num in arr if num < pivot]
-    greater_arr = [num for num in arr if num >= pivot]
+    less_arr = []
+    greater_arr = []
+    for num in arr:
+        if num < pivot:
+            less_arr.append(num)
+        else:
+            greater_arr.append(num)
     return quick_sort(less_arr) + [pivot] + quick_sort(greater_arr)
 
 if __name__ == "__main__":
