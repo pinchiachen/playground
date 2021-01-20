218617b47a5be6cc3dad1782356819102d962aee
pinchiachen <pcc@adenovo.com>
2021-01-20 11:22:52 +0800

    [2021-01-20] rename build_test_data to build_edge_test_data

--- test/quick_sort_test.py
+++ test/quick_sort_test.py
@@ -2,7 +2,7 @@ from ..utils.cal_run_time import calculate_run_time
 from ..sort.quick_sort import quick_sort
 from ..sort.quick_sort_pro import quick_sort_pro
 
-def build_test_data():
+def build_edge_test_data():
     arr = [i for i in range(800)]
     return arr
 
@@ -15,11 +15,11 @@ def quick_sort_pro_test(arr):
     return quick_sort_pro(arr)
 
 def main():
-    test_arr = build_test_data()
+    test_arr = build_edge_test_data()
     print('---Start quick sort---')
     quick_sort_test(test_arr)
     print('---End quick sort---')
-    test_arr = build_test_data()
+    test_arr = build_edge_test_data()
     print('---Start quick sort pro---')
     quick_sort_pro_test(test_arr)
     print('---End quick sort pro---')
